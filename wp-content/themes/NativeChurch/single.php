7c6d825dbdc157de833c6866292eb334b5943476
Jeff Douglas <jeff@jeffdouglas.com>
2014-08-23 07:33:08 -0400

    removed featured image

--- wp-content/themes/NativeChurch/single.php
+++ wp-content/themes/NativeChurch/single.php
@@ -19,13 +19,6 @@ the_author_meta('user_nicename', $author_id); ?></span><span><i class="fa fa-arc
                         _e('Categories: ', 'framework');
                         echo '<a href="' . get_category_link($cats[0]->term_id) . '">' . $cats[0]->name . '</a>';
                         ?> </span></span>
-                <div class="featured-image">
-                    <?php
-                    if (has_post_thumbnail()):
-                        the_post_thumbnail('full');
-                    endif;
-                    ?>
-                </div>
                 <?php
                 while (have_posts()):the_post();
                     the_content();
